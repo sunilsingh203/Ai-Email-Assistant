f10b77fc8de5f4b2469db442ab602c43d297d2bd
Sunil Singh <sunilsingh81188@gmail.com>
2025-07-13 14:53:56 +0530

    Initial commit

---
+++ email-writter-sb/src/main/java/com/email/email_writter/EmailWritterSbApplication.java
@@ -0,0 +1,13 @@
+package com.email.email_writter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailWritterSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWritterSbApplication.class, args);
+	}
+
+}
---
+++ email-writter-sb/src/test/java/com/email/email_writter/EmailWritterSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.email_writter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailWritterSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
